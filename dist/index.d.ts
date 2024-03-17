88684d756e12f9804224bef441021905e7cea62b
Michael Gorbachev <tricking.mix@gmail.com>
2024-03-18 00:40:18 +0300

    update version 3

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { LanguageModule } from './language/language.module';
-export { LanguageTypeormService } from './language/service/language-typeorm.service';
-export * from './language/interfaces';
-//# sourceMappingURL=index.d.ts.map
--- dist/language/language.module.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class LanguageModule {
-}
-//# sourceMappingURL=language.module.d.ts.map
